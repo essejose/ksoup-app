ffdbf598b022976e8bf82f2522c63cea196d7f6d
Jose <ze_pk2@hotmail.com>
2017-06-19 22:22:25 -0300

    ignore node

---
+++ servidor/server.js
@@ -0,0 +1,29 @@
+var express     = require("express");
+var bodyParser  = require("body-parser");
+var app         = express();
+
+
+app.use(bodyParser.urlencoded({ extended:false }));
+
+app.use(bodyParser.json());
+
+
+var carros = [];
+
+app.get('/carros',function(req,res){
+    res.send(carros);
+
+});
+
+app.post('/carros',function(req,res){
+    console.log(req);
+    carros.push(req.body);
+    res.send(201);
+});
+
+
+app.listen(3000,function(){
+    console.log('Servidor inciado na porta 3000');
+});
+
+ 
